59d92b3313e38fa805fc20f88b0babf669f9fdb4
U_Alice <umugwanezaalice22@gmail.com>
2024-08-06 00:05:31 +0200

    [fix]: dir name

--- src/main/java/qt/rw/challenge/blog-post/BlogPostApplication.java
+++ src/main/java/qt/rw/challenge/blog-post/BlogPostApplication.java
@@ -4,26 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import rca.ac.year3.security_starter.enums.Erole;
-import rca.ac.year3.security_starter.models.Role;
 import rca.ac.year3.security_starter.repository.IRoleRepository;
 import rca.ac.year3.security_starter.services.IRoleService;
 
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 @SpringBootApplication
-public class SecurityStarterApplication {
+public class BlogPostApplication {
 	private final IRoleRepository roleRepository;
 	private final IRoleService roleService;
 
-    public SecurityStarterApplication(IRoleRepository roleRepository, IRoleService roleService) {
+    public BlogPostApplication(IRoleRepository roleRepository, IRoleService roleService) {
         this.roleRepository = roleRepository;
         this.roleService = roleService;
     }
 
     public static void main(String[] args) {
-		SpringApplication.run(SecurityStarterApplication.class, args);
+		SpringApplication.run(BlogPostApplication.class, args);
 	}
 	@Bean
 	public String insertRolesFromEnum(){
